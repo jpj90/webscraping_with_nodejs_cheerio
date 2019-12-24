906d9727cf92a6d7fb5ab33f4b996f96087a01b5
jpj90 <jpauljanse@gmail.com>
2019-12-24 21:17:43 +0100

    slight tweak to variable name

--- scrape.js
+++ scrape.js
@@ -5,8 +5,8 @@ request('https://www.hypoport.nl/who-we-are/team/', (error, response, html) => {
     if(!error && response.statusCode == 200) {
         const $ = cheerio.load(html);
 
-        const items = $('.et_pb_team_member');
-        items.each((i, el) => { 
+        const employees = $('.et_pb_team_member');
+        employees.each((i, el) => { 
             const name = $(el).find('h4').text();
             const email = $(el).next().find('a').text();
             console.log(`employee name: ${name}, employee e-mail: ${email || 'none'}`);
